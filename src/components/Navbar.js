483621834b90dc4869496032be8e0c9d7e337a3b
Naggenda <naggendajoshua@gmail.com>
2022-06-14 00:48:21 +0300

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+// import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import '../App.css';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="logo-sect">
+              <span><Link to="/" className="logo">Bookstore CMS</Link></span>
+           </div>
+           <div className="nav-links">
+              <ul>
+                 <li><Link to="/Books" className="links"> Books </Link></li>
+                 <li><Link to="/Category" className="links"> Category </Link></li>
+              </ul>  
+           </div>
+           <div className="user">
+              <Link to="/User" className="user-img">  </Link>
+           </div>
+        </div>
+    )
+}
+
+export default Navbar
