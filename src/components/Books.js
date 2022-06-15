150e3f72c27a8ac9f3db4ba94f096f293fca50d1
Naggenda <naggendajoshua@gmail.com>
2022-06-15 17:12:30 +0300

    adding form

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react';
+import '../index.css'
 
 const Books = () => {
+    const[myArray, updateMyArray] = useState([]);
+    const onClick = () => {
+        updateMyArray(arr => [...arr, `${arr.length}`]);
+    };
+
+
+    // const[new_book] = useState(myArray);
+    const handleDelete = () =>{
+        updateMyArray(myArray.filter(arr => arr!==arr))
+    }
+    
     return (
+        <div className="addbooks">
         <div>
-            
+           {
+               myArray.map(e => 
+                   
+                <div className="new-book">
+
+                   {e}
+                   <br />
+                   <button type="button">Edit</button>
+                   <button type="button" onClick={ handleDelete }>Delete</button>
+                </div>
+                   
+               
+            )}
+        </div>
+
+        <div className="form">
+            <h3 className="header3">ADD NEW BOOK</h3>
+            <form>
+                <input type="text" placeholder="Book Title" />
+                <select>
+                   <option>Action</option>
+                   <option>Science Friction</option>
+                   <option>Economy</option>
+                </select>
+                <button type="button" className="btn-add" onClick={ onClick } value="Update">Add Book</button>
+            </form>
+        </div>
         </div>
     )
 }
